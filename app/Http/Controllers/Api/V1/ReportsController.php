90e01bfac781d5d09a2193a63b415ef3277d1d68
Andrew Esteves <esteves.andrew@gmail.com>
2018-11-12 20:43:02 -0300

    Endpoint for reports, only retrieving by authenticated user.

---
+++ app/Http/Controllers/Api/V1/ReportsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+
+use App\Models\User;
+use App\Models\Team;
+
+class ReportsController extends Controller
+{
+    /**
+     * Reports board.
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if($request->has('user_id')) {
+            return response()->json([
+                'user' => User::findOrFail($request->input('user_id'))->with('objectives')->with('keyResults')->get(),
+            ]);
+        }else if($request->has('team_id')){
+            return response()->json([
+                'team' => Team::findOrFail($request->input('team_id'))->with('objectives')->get(),
+            ]);
+        }else {
+            return response()->json([
+                'user' => Auth::user()->with('objectives')->with('keyResults')->first(),
+            ]);
+        }
+    }
+}
